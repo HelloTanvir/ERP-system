d46d5ae8e6b130bcec46be1b616b944552b533bd
MuhammadTanvirHasan98 <muhammadtanvirhasan98@gmail.com>
2024-09-24 05:44:41 +0600

    Added table to the inventory page and completed ui, also added a new page of unit of measure

--- src/app/dashboard/inventory/inventory-item/page.tsx
+++ src/app/dashboard/inventory/inventory-item/page.tsx
@@ -1,3 +1,90 @@
+import { BiExport } from 'react-icons/bi';
+import { LuImport } from 'react-icons/lu';
+import { RiFolderAddLine } from 'react-icons/ri';
+
 export default function InventoryItem() {
-    return <div>Inventory Item</div>;
+    return (
+        <div>
+            {/* Header Part */}
+            <div className="flex justify-between border-2 p-2 rounded-lg bg-gradient-to-tr from-blue-100 to-white">
+                <h3 className="text-2xl text-purple-700  font-semibold">Inventory Item</h3>
+
+                {/* Action Buttons */}
+                <div className="flex gap-2">
+                    <button
+                        type="button"
+                        className="btn btn-sm  rounded-md border-purple-700 text-purple-700 transition-all  duration-500 hover:border-green-600 hover:text-green-600"
+                    >
+                        <BiExport />
+                        Export
+                    </button>
+                    <button
+                        type="button"
+                        className="btn btn-sm  rounded-md  border-purple-700 text-purple-700 transition-all  duration-500 hover:border-yellow-600 hover:text-yellow-600"
+                    >
+                        <LuImport />
+                        Import
+                    </button>
+                    <button
+                        type="button"
+                        className="btn btn-sm bg-[#682FE6] text-white px-5 hover:border-purple-700 hover:text-purple-700 transition-all  duration-500"
+                    >
+                        <RiFolderAddLine /> New
+                    </button>
+                </div>
+            </div>
+
+            {/* Table Part */}
+            <table className="table border-collapse w-full mt-6">
+                {/* head */}
+                <thead>
+                    <tr className="text-purple-700">
+                        <th className="w-[5%] text-right border border-l-0 border-t-0  border-gray-300">
+                            <input type="checkbox" className="checkbox w-4 h-4 rounded-sm" />
+                        </th>
+                        <th className="w-[15%] border border-t-0 border-gray-300">Name</th>
+                        <th className="w-[15%] border border-t-0  border-gray-300">Units left</th>
+                        <th className="border border-t-0 border-gray-300">Code/SKU</th>
+                        <th className="text-center w-[15%] border border-t-0  border-gray-300">
+                            Description
+                        </th>
+                        <th className="text-center w-[15%] border border-t-0 border-gray-300">
+                            Active/Inactive
+                        </th>
+                        <th className="text-right w-[15%] pr-8 border border-t-0 border-r-0 border-gray-300">
+                            Action
+                        </th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {/* row 1 */}
+                    <tr className="">
+                        <td className="text-right border border-l-0">
+                            <input type="checkbox" className="checkbox w-4 h-4 rounded-sm" />
+                        </td>
+                        <td className="border border-gray-300">iPhone 16</td>
+                        <td className="border border-gray-300">20000000</td>
+                        <td className="border border-gray-300">1351DFA65</td>
+                        <td className="border border-gray-300">
+                            Any kind of description according to the product
+                        </td>
+                        <td className="text-center text-green-700 border border-gray-300">True</td>
+                        <td className="border border-r-0 border-gray-300">
+                            <div className="flex gap-2 justify-end">
+                                <button
+                                    type="button"
+                                    className="btn btn-ghost btn-sm text-blue-400"
+                                >
+                                    Edit
+                                </button>
+                                <button type="button" className="btn btn-ghost btn-sm text-red-400">
+                                    Delete
+                                </button>
+                            </div>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    );
 }
---
+++ src/app/dashboard/inventory/unit-of-measure/page.tsx
@@ -0,0 +1,7 @@
+export default function UnitOfMeasure() {
+    return (
+        <div>
+            <p>Unit of Measure page!</p>
+        </div>
+    );
+}
