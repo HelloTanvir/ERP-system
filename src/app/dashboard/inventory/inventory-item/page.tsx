3f99b467fa801c3d498a686c8b4da810a17e0260
Tanvir Hossain <tanvirhossain.butex@gmail.com>
2024-09-26 17:31:25 +0600

    fixed table responsiveness issue

--- src/app/dashboard/inventory/inventory-item/page.tsx
+++ src/app/dashboard/inventory/inventory-item/page.tsx
@@ -16,7 +16,7 @@ export default function InventoryItem() {
     };
 
     return (
-        <div>
+        <div className="flex flex-col gap-6">
             {/* Header Part */}
             <div className="flex justify-between border-[3px] border-x-[1px] border-b-0 rounded-t-lg  rounded-b-none p-2 rounded-lg ">
                 <h3 className="text-2xl text-purple-700  font-semibold">Inventory Item</h3>
@@ -48,58 +48,66 @@ export default function InventoryItem() {
             </div>
 
             {/* Table Part */}
-            <table className="table border-collapse w-full mt-6">
-                {/* head */}
-                <thead>
-                    <tr className="text-purple-700">
-                        <th className="w-[5%] text-right border border-l-0 border-t-0 border-r-0  border-gray-300">
-                            <input type="checkbox" className="checkbox w-4 h-4 rounded-sm" />
-                        </th>
-                        <th className="w-[15%] border border-l-0   border-t-0  border-gray-300">
-                            Name
-                        </th>
-                        <th className="w-[15%] border border-t-0  border-gray-300">Units left</th>
-                        <th className="border border-t-0 border-gray-300">Code/SKU</th>
-                        <th className="text-center w-[15%] border border-t-0  border-gray-300">
-                            Description
-                        </th>
-                        <th className="text-center w-[15%] border border-t-0 border-gray-300">
-                            Active/Inactive
-                        </th>
-                        <th className="text-right w-[15%] pr-8 border border-t-0 border-r-0 border-gray-300">
-                            Action
-                        </th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {/* row 1 */}
-                    <tr className="">
-                        <td className="text-right border border-r-0 border-l-0">
-                            <input type="checkbox" className="checkbox w-4 h-4 rounded-sm" />
-                        </td>
-                        <td className="border border-l-0 border-gray-300">iPhone 16</td>
-                        <td className="border border-gray-300">20000000</td>
-                        <td className="border border-gray-300">1351DFA65</td>
-                        <td className="border border-gray-300">
-                            Any kind of description according to the product
-                        </td>
-                        <td className="text-center text-green-700 border border-gray-300">True</td>
-                        <td className="border border-r-0 border-gray-300">
-                            <div className="flex gap-2 justify-end">
-                                <button
-                                    type="button"
-                                    className="btn btn-ghost btn-sm text-blue-400"
-                                >
-                                    Edit
-                                </button>
-                                <button type="button" className="btn btn-ghost btn-sm text-red-400">
-                                    Delete
-                                </button>
-                            </div>
-                        </td>
-                    </tr>
-                </tbody>
-            </table>
+            <div className="overflow-x-auto">
+                <table className="table border-collapse w-full">
+                    {/* head */}
+                    <thead>
+                        <tr className="text-purple-700">
+                            <th className="text-right border border-l-0 border-t-0 border-r-0  border-gray-300">
+                                <input type="checkbox" className="checkbox w-4 h-4 rounded-sm" />
+                            </th>
+                            <th className="border border-l-0   border-t-0  border-gray-300">
+                                Name
+                            </th>
+                            <th className="border border-t-0  border-gray-300">Units left</th>
+                            <th className="border border-t-0 border-gray-300">Code/SKU</th>
+                            <th className="text-center border border-t-0  border-gray-300">
+                                Description
+                            </th>
+                            <th className="text-center border border-t-0 border-gray-300">
+                                Active/Inactive
+                            </th>
+                            <th className="text-right pr-8 border border-t-0 border-r-0 border-gray-300">
+                                Action
+                            </th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {/* row 1 */}
+                        <tr className="">
+                            <td className="text-right border border-r-0 border-l-0">
+                                <input type="checkbox" className="checkbox w-4 h-4 rounded-sm" />
+                            </td>
+                            <td className="border border-l-0 border-gray-300">iPhone 16</td>
+                            <td className="border border-gray-300">20000000</td>
+                            <td className="border border-gray-300">1351DFA65</td>
+                            <td className="border border-gray-300">
+                                Any kind of description according to the product
+                            </td>
+                            <td className="text-center text-green-700 border border-gray-300">
+                                True
+                            </td>
+                            <td className="border border-r-0 border-gray-300">
+                                <div className="flex gap-2 justify-end">
+                                    <button
+                                        type="button"
+                                        className="btn btn-ghost btn-sm text-blue-400"
+                                    >
+                                        Edit
+                                    </button>
+                                    <button
+                                        type="button"
+                                        className="btn btn-ghost btn-sm text-red-400"
+                                    >
+                                        Delete
+                                    </button>
+                                </div>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+
             <Modal ref={modalRef} />
         </div>
     );
